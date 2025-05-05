5c04e34e97a14416d4161fc80ecec75fde00c12f
Aldo Leka <aldo.leka@live.com>
2025-05-05 14:21:15 +0200

    Add turnstile widget to login page

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -6,16 +6,17 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Turnstile } from '@marsidev/react-turnstile'
 
 export default function SignInPage() {
     return (
         <div className="flex items-center justify-center h-screen">
             <Card className="w-[400px]">
                 <CardHeader>
-                    <CardTitle>Sign in or Sign up</CardTitle>
+                    <CardTitle>Sign in / Sign up</CardTitle>
                     <CardDescription>
                         Sign in or sign up with a link straight to your inbox.
                     </CardDescription>
@@ -25,6 +26,7 @@ export default function SignInPage() {
                         <Label htmlFor="email">Email</Label>
                         <Input id="email" placeholder="email@example.com" />
                     </div>
+                    <Turnstile siteKey={process.env.TURNSTILE_SITE_KEY || ""} />
                 </CardContent>
                 <CardFooter>
                     <Button>Send Magic Link</Button>
