f8b9040dfdf3b0ee5fb698fc3fdf127369bf877a
Aldo Leka <aldo.leka@live.com>
2025-05-05 11:05:02 +0200

    Add sign in component

---
+++ client/app/create/page.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export default function CreatePage() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Card className="w-[400px]">
+                <CardHeader>
+                    <CardTitle>Sign in or Sign up</CardTitle>
+                    <CardDescription>
+                        Sign in or sign up with a link straight to your inbox.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                    <div className="space-y-1">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" placeholder="email@example.com" />
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Button>Send Magic Link</Button>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+}
