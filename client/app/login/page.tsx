85ea30854b7ef49d9b6b729c9554b73640c438cb
Aldo Leka <aldo.leka@live.com>
2025-05-06 20:36:03 +0200

    Redirecting to set url when logging in

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
         const { error } = await supabase.auth.signInWithOtp({
             email: email,
             options: {
-                emailRedirectTo: `${window.location.origin}/auth/callback`
+                emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
             }
         })
 
