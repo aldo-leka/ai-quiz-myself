6745c35169b2f59d3eef031457b04a7082e8ae82
Aldo Leka <aldo.leka@live.com>
2025-09-15 16:48:55 +0200

    remove supabase

--- client/app/create/page.tsx
+++ client/app/create/page.tsx
@@ -1,38 +1,36 @@
 "use client"
 
 import {useEffect, useState} from "react"
-import {User} from "@supabase/supabase-js"
-import { createClient } from "@/utils/supabase/client"
 import {useRouter} from "next/navigation";
 
 export default function CreatePage() {
-    const [user, setUser] = useState<User | null>(null)
-    const router = useRouter();
-    const supabase = createClient()
-
-    useEffect(() => {
-        const checkUser = async () => {
-            const { data: { session } } = await supabase.auth.getSession();
-            setUser(session?.user || null);
-        }
-
-        checkUser()
-    }, []);
-
-    const handleSignOut = async () => {
-        await supabase.auth.signOut();
-        router.push('/login');
-    };
+    // const [user, setUser] = useState<User | null>(null)
+    // const router = useRouter();
+    // const supabase = createClient()
+    //
+    // useEffect(() => {
+    //     const checkUser = async () => {
+    //         const { data: { session } } = await supabase.auth.getSession();
+    //         setUser(session?.user || null);
+    //     }
+    //
+    //     checkUser()
+    // }, []);
+    //
+    // const handleSignOut = async () => {
+    //     await supabase.auth.signOut();
+    //     router.push('/login');
+    // };
 
     return (
         <>
             Create Game
-            {user && (
+            {/*user && (
                 <button onClick={handleSignOut}>Sign out</button>
             )}
             {!user && (
                 <button onClick={() => router.push('/login')}>Sign in</button>
-            )}
+            )*/}
         </>
     )
 }
