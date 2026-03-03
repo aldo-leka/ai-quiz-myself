b4aeeea6da79eb028afb8b9810cd99f79100fcd9
Aldo <aldo.leka@live.com>
2026-03-03 11:00:28 +0100

    Remove legacy client and server folders

--- server/constants.js
+++
@@ -1,7 +0,0 @@
-// Maximum number of sentences allowed in host responses
-export const MAX_HOST_SENTENCES = 2
-
-// Number of quizzes to generate daily for caching
-export const DAILY_QUIZ_GENERATION_COUNT = 10
-
-export const GLOBAL_COUNTRY_CODE = "GLOBAL"
